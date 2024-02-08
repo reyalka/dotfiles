56e07f6a9d7816fe40dd4badc499c71035c0fd59
reyalka <140703515+reyalka@users.noreply.github.com>
2024-02-09 08:40:20 +0900

    クリアしたりformatしたり

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,17 +1,17 @@
 local opts = {
-  tabstop = 4,
-  shiftwidth = 4,
-  -- expandtab = true,
-  number = true,
-  swapfile = false,
-  ambiwidth = "single",
-  signcolumn = "yes",
-  history = 100,
-  termguicolors = true,
+	tabstop = 4,
+	shiftwidth = 4,
+	-- expandtab = true,
+	number = true,
+	swapfile = false,
+	ambiwidth = "single",
+	signcolumn = "yes",
+	history = 100,
+	termguicolors = true,
 }
 
 for k, v in pairs(opts) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 vim.opt.clipboard:append({ unnamedeplus = true })
