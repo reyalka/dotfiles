68a22ffd6e13a30e2e8edc636423a6cd05d61a5a
reyalka <140703515+reyalka@users.noreply.github.com>
2024-02-13 10:41:47 +0900

    cmpをCmdlineEnterで起動するように

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -271,7 +271,7 @@ return {
     },
     {
         "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
+        event = {"InsertEnter", "CmdlineEnter"},
         dependencies = {
             { "hrsh7th/cmp-nvim-lsp" },
             { "hrsh7th/cmp-nvim-lua" },
