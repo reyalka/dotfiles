6b12db85facd255f27720956359aaf5529225644
reyalka <140703515+reyalka@users.noreply.github.com>
2024-03-29 11:02:07 +0900

    feat: pnpm-shell-completionを追加

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -80,3 +80,7 @@ function __bd
         cd ..
     end
 end
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
