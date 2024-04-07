265b2a5d5fd2d7cf2a5590fd4725c3feeb81bfde
reyalka <140703515+reyalka@users.noreply.github.com>
2024-04-07 12:49:43 +0900

    fix: completions.fishが正常に動作しない

--- script/completions.fish
+++ script/completions.fish
@@ -2,10 +2,6 @@
 
 set -l globalargv $argv
 
-function isDebug
-	test $globalargv = "--debug" && return 0 || return 1
-end
-
 function notify
     set -l mode $argv[1]
     set -l message $argv[2]
@@ -18,43 +14,35 @@ function notify
             echo "$(set_color blue --bold) $(set_color normal)$message$(set_color normal)"
         case progress
             echo "$(set_color yellow --bold)󰊾 $message...$(set_color normal)"
-		case debug
-			isDebug && echo "$(set_color cyan --bold) $message$(set_color normal)"
     end
 end
 
 function exist
     set -l command $argv[1]
     if type $command >/dev/null
-        exit 0
+        return 0
     else
         notify info "$command is not installed. Skip it"
-        exit 1
+        return 1
     end
 end
 
-notify progress "Adding completions"
-
-notify debug "reach before mkdir"
 notify progress "Adding completions for zellij"
-exist zellij && zellij setup --generate-coompletion fish >~/dotfiles/config/fish/completions/zellij.fish \
-    && notify succsess "Completions for zellij added" \
-    || notify error "Somthing went wrong while adding completions for zellij"
+exist zellij && zellij setup --generate-completion fish > ~/dotfiles/config/fish/completions/zellij.fish \
+	&& notify succsess "Completions for zellij added" \
+	|| notify error "Somthing went wrong while adding completions for zellij"
 
-norify debug "reach before eza"
 notify progress "Adding completions for eza"
-exist eza && curl --silent https://raw.githubusercontet.com/eza-community/eza/main/completinos/eza.fish > ~/dotfiles/config/fish/completions/eza.fish \
+exist eza && curl --silent https://raw.githubusercontent.com/eza-community/eza/main/completions/fish/eza.fish > ~/dotfiles/config/fish/completions/eza.fish \
     && notify succsess "Completions for eza added" \
     || notify error "Somthing went wrong while adding completions for eza"
 
-notify debug "reach before bun"
 notify progress "Adding completions for bun"
 exist bun && curl --silent https://raw.githubusercontent.com/oven-sh/bun/main/completions/bun.fish > ~/dotfiles/config/fish/completions/bun.fish \
     && notify succsess "Completions for bun added" \
     || notify error "Somthing went wrong while adding completions for bun"
 
-notify debug "reach before brew"
 notify progress "Adding completions for brew"
-exist brew && curl --silent https://raw.githubusercontent.com/Homebrew/homebrew-fish/master/completions/brew.fish > ~/dotfiles/config/fish/completions/brew.fish \
+exist brew && curl --silent https://raw.githubusercontent.com/Homebrew/brew/master/completions/fish/brew.fish > ~/dotfiles/config/fish/completions/brew.fish \
     && notify succsess "Completions for brew added" \
     || notify error "Somthing went wrong while adding completions for brew"
