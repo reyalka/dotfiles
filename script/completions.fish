a2c74d777e01e06faf140852914fbb14549e5418
reyalka <140703515+reyalka@users.noreply.github.com>
2024-05-04 00:10:55 +0900

    feat: lefthookの補完を追加

--- script/completions.fish
+++ script/completions.fish
@@ -1,7 +1,5 @@
 #!/usr/bin/env fish
 
-set -l globalargv $argv
-
 function notify
     set -l mode $argv[1]
     set -l message $argv[2]
@@ -9,7 +7,7 @@ function notify
         case succsess
             echo "$(set_color green --bold)✔︎ $(set_color normal)$message$(set_color normal)"
         case error
-            echo "$(set_color red --bold)✖︎ $(set_color normal)$message$(set_color normal)"
+            echo "$(set_color red --bold)✖︎ $message$(set_color normal)"
         case info
             echo "$(set_color blue --bold) $(set_color normal)$message$(set_color normal)"
         case progress
@@ -28,26 +26,31 @@ function exist
 end
 
 notify progress "Adding completions for zellij"
-exist zellij && zellij setup --generate-completion fish > ~/dotfiles/config/fish/completions/zellij.fish \
-	&& notify succsess "Completions for zellij added" \
-	|| notify error "Somthing went wrong while adding completions for zellij"
+exist zellij && zellij setup --generate-completion fish >~/dotfiles/config/fish/completions/zellij.fish \
+    && notify succsess "Completions for zellij added" \
+    || notify error "Somthing went wrong while adding completions for zellij"
 
 notify progress "Adding completions for eza"
-exist eza && curl --silent https://raw.githubusercontent.com/eza-community/eza/main/completions/fish/eza.fish > ~/dotfiles/config/fish/completions/eza.fish \
+exist eza && curl https://raw.githubusercontent.com/eza-community/eza/main/completions/fish/eza.fish >~/dotfiles/config/fish/completions/eza.fish \
     && notify succsess "Completions for eza added" \
     || notify error "Somthing went wrong while adding completions for eza"
 
 notify progress "Adding completions for bun"
-exist bun && curl --silent https://raw.githubusercontent.com/oven-sh/bun/main/completions/bun.fish > ~/dotfiles/config/fish/completions/bun.fish \
+exist bun && curl --silent https://raw.githubusercontent.com/oven-sh/bun/main/completions/bun.fish >~/dotfiles/config/fish/completions/bun.fish \
     && notify succsess "Completions for bun added" \
     || notify error "Somthing went wrong while adding completions for bun"
 
 notify progress "Adding completions for brew"
-exist brew && curl --silent https://raw.githubusercontent.com/Homebrew/brew/master/completions/fish/brew.fish > ~/dotfiles/config/fish/completions/brew.fish \
+exist brew && curl --silent https://raw.githubusercontent.com/Homebrew/brew/master/completions/fish/brew.fish >~/dotfiles/config/fish/completions/brew.fish \
     && notify succsess "Completions for brew added" \
     || notify error "Somthing went wrong while adding completions for brew"
 
-notify progress "Adding completions for wezterm"\
-exist wezterm && wezterm shell-completion --shell fish > ~/dotfiles/config/fish/completions/wezterm.fish \
-	&& notify succsess "Completions for wezterm added" \
-	|| notify error "Somthing went wrong while adding completions for wezterm"
+notify progress "Adding completions for wezterm"
+exist wezterm && wezterm shell-completion --shell fish >~/dotfiles/config/fish/completions/wezterm.fish \
+    && notify succsess "Completions for wezterm added" \
+    || notify error "Somthing went wrong while adding completions for wezterm"
+
+notify progress "Adding completions for lefthook"
+exist lefthook && lefthook completion fish >~/dotfiles/config/fish/completions/lefthook.fish \
+    && notify succsess "Completions for lefthook added" \
+    || notify error "Somthing went wrong while adding completions for lefthook"
