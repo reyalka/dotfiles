4279865ec9a182fd9f85b668c31d7bc07d4f0450
reyalka <140703515+reyalka@users.noreply.github.com>
2024-12-31 16:32:30 +0900

    feat: OilとCopilotの調整
    sq

---
+++ config/nvim/lua/pluginsmulti/copilot.lua
@@ -0,0 +1,21 @@
+return {
+    "github/copilot.vim",
+    cmd = "Copilot",
+    event = "LspAttach",
+    config = function()
+        local CopilotGroup = vim.api.nvim_create_augroup("MyCopilotController", { clear = true })
+
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+            pattern = "oil",
+
+            group = CopilotGroup,
+            callback = function() vim.cmd("Copilot disable") end,
+        })
+        vim.api.nvim_create_autocmd({ "BufLeave" }, {
+            group = CopilotGroup,
+            callback = function()
+                if vim.bo.filetype == "oil" then vim.cmd("Copilot enable") end
+            end,
+        })
+    end,
+}
