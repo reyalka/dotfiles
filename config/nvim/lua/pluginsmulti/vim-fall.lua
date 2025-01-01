fac97d7b04106cbac2c34fa07577be05efeb78cd
reyalka <140703515+reyalka@users.noreply.github.com>
2025-01-01 13:32:33 +0900

    feat: disable noice.nvim while launching fall.vim
    s

--- config/nvim/lua/pluginsmulti/vim-fall.lua
+++ config/nvim/lua/pluginsmulti/vim-fall.lua
@@ -1,7 +1,27 @@
+---@class LazySpec
 return {
     "vim-fall/fall.vim",
     dependencies = {
         { "vim-denops/denops.vim" },
     },
+    keys = {
+        { "<space><space>", "<cmd>Fall file<cr>", mode = "n" },
+        { "<space>g", "<cmd>Fall rg<cr>", mode = "n" },
+    },
     cmd = "Fall",
+    config = function()
+        local FallPicker = vim.api.nvim_create_augroup("FallPicker", { clear = true })
+
+        vim.api.nvim_create_autocmd("User", {
+            group = FallPicker,
+            pattern = "FallPickerEnter:*",
+            callback = function() vim.cmd("Noice disable") end,
+        })
+
+        vim.api.nvim_create_autocmd("User", {
+            group = FallPicker,
+            pattern = "FallPickerLeave:*",
+            callback = function() vim.cmd("Noice enable") end,
+        })
+    end,
 }
