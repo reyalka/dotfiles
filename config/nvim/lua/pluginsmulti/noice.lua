ef72dbf6d1a0d14da41eaa4ca8d3c418660f5407
reyalka <140703515+reyalka@users.noreply.github.com>
2025-01-01 13:31:49 +0900

    feat: noice

--- config/nvim/lua/pluginsmulti/noice.lua
+++ config/nvim/lua/pluginsmulti/noice.lua
@@ -1,17 +1,28 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-    },
-    opts = {
-        presets = {
-            bottom_search = false, -- use a classic bottom cmdline for search
-            command_palette = false, -- position the cmdline and popupmenu together
-            long_message_to_split = false, -- long messages will be sent to a split
-            inc_rename = false, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = false, -- add a border to hover docs and signature help
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
         },
-        lsp = { progress = { enabled = false } },
+        opts = {
+            presets = {
+                bottom_search = false, -- use a classic bottom cmdline for search
+                command_palette = false, -- position the cmdline and popupmenu together
+                long_message_to_split = false, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+            },
+            lsp = { progress = { enabled = false } },
+        },
+    },
+    {
+        "rcarriga/nvim-notify",
+        event = "VeryLazy",
+        opts = {
+            render = "compact",
+            stages = "slide",
+            timeout = 3000,
+        },
     },
 }
