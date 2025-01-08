0c820213dfe80eedaac8b4132f764d6d6240d498
reyalka <140703515+reyalka@users.noreply.github.com>
2025-01-08 21:16:30 +0900

    feat: nekonightつらい

--- config/nvim/lua/plugins/colorsheme.lua
+++ config/nvim/lua/plugins/colorsheme.lua
@@ -1,19 +1,7 @@
 return {
-    "BrunoCiccarino/nekonight",
+    "folke/tokyonight.nvim",
+    -- "BrunoCiccarino/nekonight",
     lazy = false,
     priority = 1000,
-    config = function()
-        require("nekonight").setup({
-            on_colors = function(colors) colors.bg_float = colors.bg end,
-        })
-
-        vim.cmd("colorscheme nekonight")
-
-        local highlights = {
-            Front = { bg = "#4c3f5c" },
-        }
-        for gorup, hl in pairs(highlights) do
-            vim.api.nvim_set_hl(0, gorup, hl)
-        end
-    end,
+    config = function() vim.cmd.colorscheme("tokyonight") end,
 }
