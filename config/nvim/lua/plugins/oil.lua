158206fdd9d8a1897aeb3c1cbd802431f506d1a7
reyalka <140703515+reyalka@users.noreply.github.com>
2025-02-04 22:58:46 +0900

    feat: oilを開いたときのいろいろ

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -2,12 +2,40 @@
 return {
     "stevearc/oil.nvim",
     cmd = "Oil",
-    opts = {
-        view_options = {
-            show_hidden = true,
-        },
-        float = {
-            padding = 6,
-        },
-    },
+    config = function()
+        require("oil").setup({
+            view_options = {
+                show_hidden = true,
+            },
+            float = {
+                padding = 6,
+            },
+        })
+
+        local OilGroup = vim.api.nvim_create_augroup("OilGroup", { clear = false })
+
+        -- q or <esc> to close oil
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "oil",
+            callback = function ()
+                vim.keymap.set("n", "q", "<cmd>lua require('oil').close()<CR>", { buffer = 0 })
+                vim.keymap.set("n", "<esc>", "<cmd>lua require('oil').close()<CR>", { buffer = 0 })
+                vim.keymap.set("n", "<space>w", "<nop>", { buffer = 0 })
+            end,
+            group = OilGroup
+        })
+
+        -- disable copilot when oil is open
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+            pattern = "oil",
+            group = OilGroup,
+            callback = function() vim.cmd("Copilot disable") end,
+        })
+        vim.api.nvim_create_autocmd({ "BufLeave" }, {
+            group = OilGroup,
+            callback = function()
+                if vim.bo.filetype == "oil" then vim.cmd("Copilot enable") end
+            end,
+        })
+    end
 }
