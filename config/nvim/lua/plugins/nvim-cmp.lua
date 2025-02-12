370b413c6a677ed2379a1ee6d7f4834c0a1791ad
reyalka <140703515+reyalka@users.noreply.github.com>
2025-02-12 22:37:23 +0900

    feat: cmp-calcは使わない

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -7,7 +7,6 @@ return {
         { "hrsh7th/cmp-nvim-lua" },
         { "hrsh7th/cmp-path" },
         { "hrsh7th/cmp-cmdline" },
-        { "hrsh7th/cmp-calc" },
         { "onsails/lspkind.nvim" },
         { "saadparwaiz1/cmp_luasnip" },
         { "L3MON4D3/LuaSnip" },
@@ -34,21 +33,6 @@ return {
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
                 { name = "path" },
-                { name = "calc" },
-            }),
-            cmp.setup.cmdline(":", {
-                mapping = cmp.mapping.preset.cmdline(custom_mapping),
-                sources = {
-                    { name = "cmdline" },
-                    { name = "path" },
-                },
-            }),
-            cmp.setup.cmdline({ "/", "?" }, {
-                mapping = cmp.mapping.preset.cmdline(custom_mapping),
-                sources = {
-                    { name = "path" },
-                    { name = "buffer" },
-                },
             }),
             formatting = {
                 format = lspkind.cmp_format({
@@ -59,6 +43,28 @@ return {
                 expandable_indicator = true,
             },
             mapping = cmp.mapping.preset.insert(custom_mapping),
+        })
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(custom_mapping),
+            sources = cmp.config.sources({
+                { name = "cmdline" },
+                { name = "path" },
+            }),
+        })
+        cmp.setup.cmdline({ "/", "?" }, {
+            mapping = cmp.mapping.preset.cmdline({
+                ["<C-k>"] = cmp.mapping.select_prev_item(),
+                ["<C-j>"] = cmp.mapping.select_next_item(),
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.close(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = {
+                { name = "path" },
+                { name = "buffer" },
+            },
         })
     end,
 }
