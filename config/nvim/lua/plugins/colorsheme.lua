081e5096d9d0de38b52b7c6c516c28839f7a1bce
reyalka <140703515+reyalka@users.noreply.github.com>
2025-03-16 14:10:32 +0900

    feat: colorschemeの切り替えを実装

--- config/nvim/lua/plugins/colorsheme.lua
+++ config/nvim/lua/plugins/colorsheme.lua
@@ -1,7 +1,35 @@
-return {
-    "folke/tokyonight.nvim",
-    -- "BrunoCiccarino/nekonight",
-    lazy = false,
-    priority = 1000,
-    config = function() vim.cmd.colorscheme("tokyonight") end,
+---@type LazySpec[]
+local colorschemes = {
+    tokyonight = {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function() vim.cmd.colorscheme("tokyonight") end,
+    },
+    ayu = {
+        "ayu-theme/ayu-vim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("ayu")
+            vim.g.ayucolor = "mirage"
+        end,
+    },
+    gruvbox = {
+        "morhetz/gruvbox",
+        lazy = false,
+        priority = 1000,
+        config = function() vim.cmd.colorscheme("gruvbox") end,
+    },
+    nekonight = {
+        "BrunoCiccarino/nekonight",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function ()
+           vim.cmd.colorscheme("nekonight-moon") 
+        end
+    },
 }
+
+return colorschemes.tokyonight
