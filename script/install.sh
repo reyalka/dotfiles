1a3c89fbeaea821a86d335c57613a9aaf2d598f2
reyalka <140703515+reyalka@users.noreply.github.com>
2025-04-03 23:22:13 +0900

    feat: lazygit,仲間はずれにしてごめんね

--- script/install.sh
+++ script/install.sh
@@ -8,6 +8,7 @@ ln -svf ~/dotfiles/config/fish ~/.config/
 ln -svf ~/dotfiles/config/wezterm/ ~/.config/
 ln -svf ~/dotfiles/config/aqua/ ~/.config/
 ln -svf ~/dotfiles/config/zellij/ ~/.config/
+ln -svf ~/dotfiles/config/lazygit/ ~/.config/
 ln -svf ~/dotfiles/config/starship.toml ~/.config/
 ln -svf ~/dotfiles/config/.zshrc ~/
 
