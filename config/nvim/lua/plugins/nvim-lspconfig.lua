20c18dfa723ea72f48ccfc22ec799a463ae31bef
reyalka <140703515+reyalka@users.noreply.github.com>
2025-11-05 00:17:35 +0900

    feat: 設定終わり

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -30,99 +30,44 @@ return {
     },
     event = { "BufRead", "BufNewFile" },
     config = function()
-        local lspconfig = require("lspconfig")
         require("lspsaga").setup({})
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-        -- capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+        local function on_attach(client, bufnr)
+            local set = vim.keymap.set
+            set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { buffer = bufnr, desc = "Go to Definition" })
+            set("n", "gr", "<cmd>Lspsaga finder<CR>", { buffer = bufnr, desc = "References" })
+            set("n", "ga", "<cmd>Lspsaga code_action<CR>", { buffer = bufnr, desc = "Code Action" })
+            set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { buffer = bufnr, desc = "Hover Documentation" })
+            set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { buffer = bufnr, desc = "Rename" })
+            set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { buffer = bufnr, desc = "Previous Diagnostic" })
+            set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", { buffer = bufnr, desc = "Next Diagnostic" })
+
+            if client.server_capabilities.inlayHintProvider then
+                vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+            end
+        end
+
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    runtime = { version = "LuaJIT", },
+                    diagnostics = { globals = { "vim" }, },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false,
+                    },
+                },
+            }
+        })
 
         vim.lsp.config("*", {
             capabilities = capabilities,
+            on_attach = on_attach,
         })
         vim.lsp.enable(servers)
-        -- local function is_node_dir()
-        --     return lspconfig.util.root_pattern("package.json")(vim.fn.getcwd())
-        -- end
-        --
-        -- local server_configs = {
-        --     lua_ls = {
-        --         settings = {
-        --             Lua = {
-        --                 runtime = {
-        --                     version = "LuaJIT",
-        --                 },
-        --                 diagnostics = {
-        --                     globals = { "vim" },
-        --                 },
-        --                 workspace = {
-        --                     library = vim.api.nvim_get_runtime_file("", true),
-        --                     checkThirdParty = false,
-        --                 },
-        --             },
-        --         },
-        --     },
-        --     tsserver = {
-        --         on_attach = function(client, bufnr)
-        --             on_attach(client, bufnr)
-        --             if not is_node_dir() then
-        --                 client.stop(true)
-        --             end
-        --         end,
-        --     },
-        --     denols = {
-        --         on_attach = function(client, bufnr)
-        --             on_attach(client, bufnr)
-        --             if is_node_dir() then
-        --                 client.stop(true)
-        --             end
-        --         end,
-        --     },
-        --     tailwindcss = {
-        --         filetypes = {
-        --             "astro",
-        --             "javascriptreact",
-        --             "typescriptreact",
-        --             "svelte",
-        --             "vue",
-        --         },
-        --     },
-        --     emmet_language_server = {
-        --         filetypes = {
-        --             "astro",
-        --             "css",
-        --             "html",
-        --             "javascript",
-        --             "javascriptreact",
-        --             "scss",
-        --             "typescript",
-        --             "typescriptreact",
-        --         },
-        --     },
-        -- }
-        --
-        -- for _, server_name in ipairs(servers) do
-        --     local config = {
-        --         on_attach = on_attach,
-        --         capabilities = capabilities,
-        --     }
-        --     if server_configs[server_name] then
-        --         config = vim.tbl_deep_extend("force", config, server_configs[server_name])
-        --     end
-        --     lspconfig[server_name].setup(config)
-        -- end
-
-        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-            virtual_text = {
-                format = function(diagnostic)
-                    if diagnostic.code then
-                        return string.format("%s [%s](%s)", diagnostic.message, diagnostic.source, diagnostic.code)
-                    end
-                    return string.format("%s [%s]", diagnostic.message, diagnostic.source)
-                end,
-                update_in_insert = true,
-            },
-        })
 
         vim.diagnostic.config({
             severity_sort = true,
