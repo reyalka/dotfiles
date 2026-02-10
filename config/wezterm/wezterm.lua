6b7d34fbb24d02349f324085af75fb3fce1c3c19
reyalka <140703515+reyalka@users.noreply.github.com>
2026-02-10 22:29:02 +0900

    feat: wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,10 +1,22 @@
+---@type Wezterm
 local wezterm = require("wezterm")
 
-local config = {}
+---@type Config
+local config = wezterm.config_builder()
 
 config.use_ime = true
 config.front_end = "OpenGL"
 config.color_scheme = "Everblush"
 config.enable_wayland = false
+-- config.default_domain = "WSL:NixOS"
+config.automatically_reload_config = true
+config.font_size = 16.0
+config.window_background_opacity = 0.85
+config.window_decorations = "RESIZE"
+config.window_frame = {
+    inactive_titlebar_bg = "none",
+    active_titlebar_bg = "none",
+}
+config.show_new_tab_button_in_tab_bar = false
 
 return config
